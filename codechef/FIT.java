ce106d5ac417bda78a0aa9bfbc5349e549d71e17
Renuka <pathivadarenuka8@gmail.com>
2025-03-10 19:54:05 +0530

    Add files via upload

---
+++ codechef/FIT.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        
+        while (t-- > 0) {
+            int x = scanner.nextInt();
+            // Your code goes here
+            System.out.println(5*2*x);
+            
+        }
+    }
+}
