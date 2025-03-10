4e907025651da8ddec211d26717dd9b2012decb1
renuka <pathivadarenuka8@gmail.com>
2025-03-10 19:51:03 +0530

    changes

---
+++ codechef/PRACTICEPERF.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int P1=sc.nextInt();
+		int P2=sc.nextInt();
+		int P3=sc.nextInt();
+		int P4=sc.nextInt();
+		int c=0;
+		if(P1>=10){
+		    c++;
+		}
+		if(P2>=10){
+		    c++;
+		    
+		}
+		if(P3>=10){
+		    c++;
+		}
+		if(P4>=10){
+		    c++;
+		}
+		System.out.println(c);
+	}
+}
